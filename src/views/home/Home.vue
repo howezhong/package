334ab2da3a897b7827bdcce0379c2374b6f18993
howezhong <zanely163@163.com>
2019-10-28 21:29:27 +0800

    增加渐变字体ie不支持用svg做的示例

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home-container clearfix">
+    <p class="desc-tip">因为IE兼容性问题, 渐变会无效, 顾改成svg方式</p>
     <p class="mi-title">组件封装示例</p>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBoxs="0 0 280 100" width="280" height="100">
+      <defs>
+        <linearGradient id="svgTitle" gradientUnits="userSpaceOnUse" x1="0%" y1="0%" x2="0%" y2="100%">
+          <stop offset="0%" style="stop-color:#ffecc8"/>
+          <stop offset="75%" style="stop-color:#ffc37a"/>
+        </linearGradient>
+      </defs>
+      <text class="gradient-text" text-anchor="middle" x="50%" y="50%" dy=".35em">组件封装示例</text>
+    </svg>
   </div>
 </template>
 
@@ -18,5 +28,8 @@ export default {
   font-size: 30px;
   padding-top: 5%;
   text-align: center;
+}
+.desc-tip {
+  padding-bottom: 25px;
 }
 </style>
